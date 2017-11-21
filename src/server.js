bbb7104ac95c160d37c07011b23d199497d3a31d
hoslackochieng@gmail.com <hoslackochieng@gmail.com>
2017-11-21 14:22:36 +0200

    Server created Reference #5

---
+++ src/server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var handler = require('./handler');
+var port = 3000; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var server = http.createServer(handler);
+
+server.listen(port, function () {
+	console.log('Server is listening at ' + port);
+})
+
+
