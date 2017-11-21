30f998bbcaa36b70ea5bef977ff5844de981b730
prodionov <pavelrodionov@hotmail.com>
2017-11-21 18:09:07 +0200

    read the words.txt file from handlers.js Reference #12

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,15 @@
 var fs = require('fs');
+var arr = [];
 
+fs.readFile(__dirname + '/words.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  else {
+    arr = data.toString().split("\n");
+    console.log(arr);
+  }
+})
 
 function handler(request, response) {
   var url = request.url;
@@ -52,9 +62,7 @@ function handler(request, response) {
       }
     });
 
-  }
-else if(url==='/autoComplete'){
-
+  } else if(url==='/autoComplete'){
 		response.writeHead(200, {'Content-Type':'text/html'});
 		response.end('xhr works');
 
