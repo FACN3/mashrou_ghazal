f23a0f58fdf8b938b161b37c55e73e2f1cacb5ba
Ghassan <ghassan.maslamani@gmail.com>
2017-11-21 15:05:43 +0200

    Response to the requests that comes from the index.html file
    relates #7

--- src/handler.js
+++ src/handler.js
@@ -1,23 +1,62 @@
 var fs = require('fs');
 
 
-function handler(request, response){
+function handler(request, response) {
   var url = request.url;
-  if(url === '/') {
+  if (url === '/') {
     console.log('path', __dirname + '/../public/index.html');
     fs.readFile(__dirname + '/../public/index.html', (err, data) => {
-      if (err){
-        response.writeHead(500, {'Content-Type':'text/html'});
+      if (err) {
+        response.writeHead(500, {
+          'Content-Type': 'text/html'
+        });
         response.end('Internal Server Error');
-      }
-      else {
-        response.writeHead(200, {'Content-Type':'text/html'})
+      } else {
+        response.writeHead(200, {
+          'Content-Type': 'text/html'
+        })
         response.end(data);
       }
     });
-  }
-  else {
-    response.writeHead(404, {'Content-Type':'text/html'});
+  } else if (url === '/css/style.css') {
+    fs.readFile(__dirname + '/../public' + url, (err, data) => {
+
+      if (err) {
+        response.writeHead(500, {
+          'Content-Type': 'text/html'
+        });
+        response.end('Internal Server Error');
+      } else {
+        response.writeHead(200, {
+          'Content-Type': 'text/css'
+        })
+        response.end(data);
+
+      }
+
+    });
+
+  } else if (url === '/js/index.js') {
+    fs.readFile(__dirname + '/../public' + url, (err, data) => {
+
+      if (err) {
+        response.writeHead(500, {
+          'Content-Type': 'text/html'
+        });
+        response.end('Internal Server Error');
+      } else {
+        response.writeHead(200, {
+          'Content-Type': 'application/javascript'
+        })
+        response.end(data);
+      }
+    });
+
+  } else {
+    console.log('URL is ', url);
+    response.writeHead(404, {
+      'Content-Type': 'text/html'
+    });
     response.end('404 not found');
   }
 
