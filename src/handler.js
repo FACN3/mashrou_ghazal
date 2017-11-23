5b5b341fc229af431f3587a517e5d45a66d6cb90
Ghassan <ghassan.maslamani@gmail.com>
2017-11-23 12:06:15 +0200

    adding a router.js file to the src folder
    relates #47

--- src/handler.js
+++ src/handler.js
@@ -13,45 +13,34 @@ fs.readFile(__dirname + '/words.txt', (err, data) => {
 })
 
 
-function handler(request, response) {
-  var url = request.url;
-  response_path = {
-    '/': '/../public/index.html',
-    '/css/style.css': '/../public' + url,
-    '/js/index.js': '/../public' + url
-  }[url]
 
 
-  contentType = {
-    '/': 'text/html',
-    '/css/style.css': 'text/css',
-    '/js/index.js': 'application/javascript'
-  }[url]
+function not_found(request,response){
+  response.writeHead(404, {
+    'Content-Type': 'text/html'
+  });
+  response.end('404 not found');
+
+
+}
+
+
+function api_handler(request,response,url){
 
   var query = urlObject.parse(url, true).query.search;
 
-  public_list = ['/', '/css/style.css', '/js/index.js'];
 
-  if (public_list.includes(url)) {
-    file_handler(url,response_path,contentType);
+      var search_result = autoComplete(arr, query);
+      response.writeHead(200, {
+        'Content-Type': 'application/javascript'
+      });
+      response.end(JSON.stringify(search_result));
 
-  } else if (url.slice(0, 13) === '/autoComplete') {
+
+}
 
 
-    var search_result = autoComplete(arr, query);
-    response.writeHead(200, {
-      'Content-Type': 'application/javascript'
-    });
-    response.end(JSON.stringify(search_result));
-
-  } else {
-    response.writeHead(404, {
-      'Content-Type': 'text/html'
-    });
-    response.end('404 not found');
-  }
-
-  function file_handler(url,response_path,contentType) {
+  function file_handler(request,response,url,response_path,contentType) {
 
 
     fs.readFile(__dirname + response_path, (err, data) => {
@@ -74,6 +63,8 @@ function handler(request, response) {
 
 
 
-}
 
-module.exports = handler;
+
+module.exports = {'file_handler':file_handler,
+                  'api_handler': api_handler,
+                    'not_found': not_found  };
